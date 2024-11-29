5749ebedbf80a0349a9e1754dc85cd72b304ceb1
Dipak Yadav <dipak.yadav.31.1@mountblue.tech>
2024-11-29 14:30:12 +0530

    Test file created for Specific Car finding

---
+++ Test/testProblem6.js
@@ -0,0 +1,8 @@
+// Imported Data
+
+const problem6 = require("../problem6");
+const carData = require("../carData");
+
+const bmwAndAudi = problem6(carData);
+
+console.log(JSON.stringify(bmwAndAudi));

d9fe77d3dfa59eb73592a8c102e23d7e4d6b07dd
Dipak Yadav <dipak.yadav.31.1@mountblue.tech>
2024-11-29 11:33:18 +0530

    Problem_1 Solved

--- Test/testProblem1.js
+++ Test/testProblem1.js
@@ -7,7 +7,7 @@
 const problem1 = require("../problem1");
 const carData = require("../carData");
 
-const result = problem1(carData, (id = 33));
+const result = problem1(carData, (id = 156));
 
 console.log(
   `Car 33 is a ${result.car_year} ${result.car_make} ${result.car_model}`

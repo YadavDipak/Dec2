783cfc99182b1ada63b844344674cc2d5c1e898b
Dipak Yadav <dipak.yadav.31.1@mountblue.tech>
2024-12-04 12:23:57 +0530

    Some another Logic Added

--- Test/testProblem5.js
+++ Test/testProblem5.js
@@ -5,3 +5,7 @@ const carData = require("../carData");
 
 const olderCars = problem5(carData);
 console.log(olderCars);
+
+console.log(
+  `Length of an array which are older than 2000 is: ${olderCars.length}`
+);
